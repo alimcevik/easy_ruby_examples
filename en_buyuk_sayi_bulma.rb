b6825384b51da614522ee18047ecd8b1019a0022
Ali Murat Çevik <alimcvk@hotmail.com>
2017-09-28 23:11:54 +0300

    sadeleştirildi

---
+++ en_buyuk_sayi_bulma.rb
@@ -0,0 +1,20 @@
+#Girilen üç sayıdan en büyüğünü bulalım
+
+puts "3 sayı giriniz: "
+a = gets.chomp.to_i
+b = gets.chomp.to_i
+c = gets.chomp.to_i
+
+max = a
+
+	if c > max 
+#ilk önce c koşuluna bakmalıyız. Eğer "b > max" şeklinde başlarsak, programımız ilk durum sağlandığı taktirde diğer seçenekleri kontrol etmeden if yapısından çıkacaktır.
+
+		max = c
+
+		elsif b > max
+			max = b
+				
+	end
+
+	puts "En büyük sayi: #{max}"
