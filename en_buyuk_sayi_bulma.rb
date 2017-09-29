5f7c02a7cd71a05b91768243c4dfee02708b6989
Ali Murat Çevik <alimcvk@hotmail.com>
2017-09-29 13:06:24 +0300

    Update en_buyuk_sayi_bulma.rb

--- en_buyuk_sayi_bulma.rb
+++ en_buyuk_sayi_bulma.rb
@@ -7,14 +7,10 @@ c = gets.chomp.to_i
 
 max = a
 
-	if c > max 
+if c > max 
 #ilk önce c koşuluna bakmalıyız. Eğer "b > max" şeklinde başlarsak, programımız ilk durum sağlandığı taktirde diğer seçenekleri kontrol etmeden if yapısından çıkacaktır.
-
-		max = c
-
-		elsif b > max
-			max = b
-				
-	end
-
-	puts "En büyük sayi: #{max}"
+	max = c
+	elsif b > max
+		max = b	
+end
+puts "En büyük sayi: #{max}"
