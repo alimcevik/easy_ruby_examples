d04919a27c9e775e90b689d9fe8bf9ce9311fb63
Ali Murat Çevik <alimcvk@hotmail.com>
2017-09-30 16:33:12 +0300

    Add files via upload

---
+++ atm_case.rb
@@ -0,0 +1,37 @@
+#Basit Bir ATM Uygulaması
+bakiye = 1000
+puts "Bakiye: #{bakiye} TL"
+puts "İşlemler:\n 1.Para Çekme\n 2.Para Yatırma\n 3.Havale \n 4.Bakiye Sorgulama"
+islem = gets.chomp.to_i
+
+ case islem
+	when 1
+	puts "Çekilecek tutarı girin: "
+	tutar = gets.chomp.to_f
+
+		if tutar > bakiye 
+			puts "Yetesiz bakiye!"
+		else
+			bakiye -= tutar
+		end
+			
+	when 2
+		puts "Yatırılacak tutarı girin: "
+		tutar = gets.chomp.to_f
+		bakiye += tutar
+
+	when 3
+		puts "Havale edilecek tutarı girin: "
+	tutar = gets.chomp.to_f
+		if tutar > bakiye
+				puts "Yetersiz Bakiye!"
+			else
+			bakiye -= tutar
+			puts "Havale Başarılı"
+		end
+				
+	else
+		puts "Yanlış işlem yaptınız! \n tekrar deneyin"
+
+	end
+	puts "\nKalan bakiyeniz: #{bakiye}"
