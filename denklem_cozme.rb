cba63869b6f06420bd2cba32889edb4822a1547e
Ali Murat Çevik <murat.cevik@bil.omu.edu.tr>
2017-10-07 00:19:45 +0300

    Update denklem_cozme.rb

--- denklem_cozme.rb
+++ denklem_cozme.rb
@@ -6,56 +6,56 @@ puts "1. Dereceden Bir Denklem \n2. Dereceden Bir Denklem\n3. Dereceden Bir Denk
 derece = gets.chomp.to_i
 
 case derece
-	when 1
-		puts "\n #{derece}. Dereceden Fonksiyon Çözümü\n Katsayı Giriniz:"
-		kat_sayi = gets.chomp.to_i
+  when 1
+  puts "\n #{derece}. Dereceden Fonksiyon Çözümü\n Katsayı Giriniz:"
+  kat_sayi = gets.chomp.to_i
 	
-	puts " Sabit Sayı Giriniz"
-		sabit = gets.chomp.to_i
+  puts " Sabit Sayı Giriniz"
+  sabit = gets.chomp.to_i
 		
-	if sabit < 0
-			isaret = ''
-		else
-			isaret = '+'
+  if sabit < 0
+  isaret = ''
+   else
+   isaret = '+'
+  end
 
-	end
-
-		puts " Oluşturulan fonksiyon = #{kat_sayi}x #{isaret} #{sabit} "
+puts " Oluşturulan fonksiyon = #{kat_sayi}x #{isaret} #{sabit} "
 	
-		print " x değerini giriniz: "
-		x = gets.chomp.to_i
+print " x değerini giriniz: "
+x = gets.chomp.to_i
 
-		fonk = (kat_sayi * x) + sabit
-		puts " Cevap: #{fonk}"
+fonk = (kat_sayi * x) + sabit
+puts " Cevap: #{fonk}"
 
-	when 2			
-		puts "\n #{derece}. Dereceden Fonksiyon Çözümü\n Katsayı Giriniz:"
-		kat_sayi = gets.chomp.to_i
+  when 2			
+  puts "\n #{derece}. Dereceden Fonksiyon Çözümü\n Katsayı Giriniz:"
+  kat_sayi = gets.chomp.to_i
 
-		puts " Sabit Sayı Giriniz"
-		sabit = gets.chomp.to_i
+  puts " Sabit Sayı Giriniz"
+  sabit = gets.chomp.to_i
+        
+  puts " Oluşturulan fonksiyon = #{kat_sayi}x²+#{sabit} "
 
-		puts " Oluşturulan fonksiyon = #{kat_sayi}x²+#{sabit} "
+  print " x değerini giriniz: "
+  x = gets.chomp.to_i
 
-		print " x değerini giriniz: "
-		x = gets.chomp.to_i
-
-		fonk = (kat_sayi * x**2) + sabit
-		puts " Cevap: #{fonk}"
+  fonk = (kat_sayi * x**2) + sabit
+  puts " Cevap: #{fonk}"
 		
-	when 3
+  when 3
+  puts "\n #{derece}. Dereceden Fonksiyon Çözümü\n Katsayı Giriniz:"
+  kat_sayi = gets.chomp.to_i
+        
+  puts " Sabit Sayı Giriniz"			
+  sabit = gets.chomp.to_i
+        
+  puts " Oluşturulan fonksiyon = #{kat_sayi}x³+#{sabit} "
+        
+  print " x değerini giriniz: "
+  x = gets.chomp.to_i
+        
+  fonk = (kat_sayi * x**3) + sabit
 
-		puts "\n #{derece}. Dereceden Fonksiyon Çözümü\n Katsayı Giriniz:"
-		kat_sayi = gets.chomp.to_i
-
-		puts " Sabit Sayı Giriniz"			
-		sabit = gets.chomp.to_i
-
-		puts " Oluşturulan fonksiyon = #{kat_sayi}x³+#{sabit} "
-		print " x değerini giriniz: "
-		x = gets.chomp.to_i
-
-		fonk = (kat_sayi * x**3) + sabit
-
-		puts " Cevap: #{fonk}"
+  puts " Cevap: #{fonk}"
+        
 end
